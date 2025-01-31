8435ad5e70d5201686da07cbf78916441541d0d1
Xiujie <a0976954403@gmail.com>
2025-01-31 16:29:51 +0800

    validation curve

--- wdbc.py
+++ wdbc.py
@@ -99,7 +99,7 @@ train_sizes, train_scores, test_scores =\
                                 X=X_train,
                                 y=y_train,
                                 train_sizes=np.linspace(    
-                                            0.1, 1.0, 10),
+                                        0.1, 1.0, 10),
                                 cv=10,
                                 n_jobs=1)
 train_mean = np.mean(train_scores, axis=1)
@@ -131,4 +131,41 @@ plt.xlabel('Number of training examples')
 plt.ylabel('Accuracy')
 plt.legend(loc='lower right')
 plt.ylim([0.8, 1.03])
+plt.show()
+
+#10.使用scikit-learn建立「驗證曲線」
+from sklearn.model_selection import validation_curve
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+train_scores, test_scores = validation_curve(
+                estimator=pipe_lr,
+                X=X_train,
+                y=y_train,
+                param_name='logisticregression__C',
+                param_range=param_range,
+                cv=10)
+train_mean=np.mean(train_scores,axis=1)
+train_std=np.std(train_scores,axis=1)
+test_mean=np.mean(test_scores,axis=1)
+test_std=np.std(test_scores,axis=1)
+plt.plot(param_range, train_mean,
+        color='blue',marker='o',
+        markersize=5, label='training accuracy')
+plt.fill_between(param_range, 
+                train_mean + train_std,
+                train_mean - train_std, 
+                alpha=0.15,color='blue')
+plt.plot(param_range, train_mean,
+        color='green', linestyle='--',
+        marker='s', markersize=5, 
+        label='validation accuracy')
+plt.fill_between(param_range,
+                test_mean + test_std,
+                test_mean - test_std,
+                alpha=0.15, color='green')
+plt.grid()
+plt.legend(loc='lower right')
+plt.xscale('log')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
 plt.show()
